2e638cdfb173a88275b4b7aceb7e605eb8f8a003
lamonte24kG <william.golden@seattlecolleges.edu>
2025-03-10 08:51:09 -0700

    Added jsonIT115.py

---
+++ jsonIT115.py
@@ -0,0 +1,16 @@
+#import json library
+import json
+
+#create the data dictionary
+
+data = {
+
+    'name': 'John Doe',
+    'age': '30',
+    'city': 'Seattle,WA',
+    'interest': ['music', 'cooking', 'walks'],
+    'is_student': True
+
+
+}
+
