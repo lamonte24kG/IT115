6d8346a1facb0474cc179bfb88c74da47138129f
lamonte24kG <william.golden@seattlecolleges.edu>
2025-03-10 08:03:38 -0700

    Initial commit

---
+++ json111.py
@@ -0,0 +1,14 @@
+#import json class library
+import json
+
+#create the data dictionary
+
+data = {
+
+    'name': 'John Doe',
+    'age':30,
+    'city':'New York,NY',
+    'interest': ['golf', 'running', 'videogames'],
+    'is_student': False
+
+}
